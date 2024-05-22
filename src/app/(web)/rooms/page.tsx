6c1cb4f2c5a6974dfccffc299cabf2f457f4fc27
hamza chbit <hamzachbit@gmail.com>
2024-05-22 18:45:34 +0000

    edit v1

--- src/app/(web)/rooms/page.tsx
+++ src/app/(web)/rooms/page.tsx
@@ -116,7 +116,7 @@ const Rooms = () => {
       
           <h1 className="mt-6 text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl">Uh-oh!</h1>
       
-          <p className="mt-4 text-gray-500">We can't find that Room.</p>
+          <p className="mt-4 text-gray-500">We can t find that Room.</p>
         </div>
       </div>
       }
