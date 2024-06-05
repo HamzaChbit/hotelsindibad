46ec5e32c7fb1c7f8b74da3dee3a88b16924b6df
hamza chbit <hamzachbit@gmail.com>
2024-06-05 15:31:01 +0000

    v14

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,15 +1,13 @@
 import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
+
   return {
-    rules: [
-      {
-        userAgent: '*',
-        allow: '/',
-        disallow:'/studio'
-      },
-   
-    ],
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      
+    },
     sitemap: 'https://hotelsindibad.ma/sitemap.xml',
-  };
+  }
 }
